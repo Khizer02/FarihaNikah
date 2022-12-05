a756de30fb5d3696b943d59a26ad5426e722852b
Khizer02 <66351822+Khizer02@users.noreply.github.com>
2022-12-05 17:27:16 +0530

    Update to jan 21

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 24, 2021 00:00:00").getTime();
+var countDownDate = new Date("Jan 21, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
